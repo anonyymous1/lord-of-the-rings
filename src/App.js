b3452c4f11a1a5d8954991b56b5954e73c37e67b
Ruben Cedeno <rubencedeno@rubens-mbp.myfiosgateway.com>
2020-12-09 09:43:47 -0500

    [App] Solution

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import Movie from './Movie'
 import './App.css';
 
 function App() {
+  const movieData = [
+    {
+      title: "The Fellowship of the Ring",
+      hours:"2",
+      minutes:"58"
+    },
+    {
+      title: "The Two Towers",
+      hours:"2",
+      minutes:"59"
+    },
+    {
+      title: "The Return of the King",
+      hours:"3",
+      minutes:"21"
+    },
+  ]
+
+  const movieComponents = movieData.map((d, index)=>{
+    return <Movie { ...d } key={'movie-'+ index}/>
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,9 +32,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Movie title="The Fellowship of the Ring" hours="2" min="58"/>
-        <Movie title="The Two Towers" hours="2" min="59"/>
-        <Movie title="The Return of the King" hours="3" min="21"/>
+                { movieComponents }
       </header>
     </div>
   );
